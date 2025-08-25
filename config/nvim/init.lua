1c75dbf2f7d30b357672ab1391446e56d450533a
kiëst <147881592+kiestdev@users.noreply.github.com>
2025-08-25 17:58:43 -0600

    Add files via upload

---
+++ config/nvim/init.lua
@@ -0,0 +1,46 @@
+vim.g.mapleader = " "
+
+vim.o.number = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim", -- REQUIRED
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = { follow_current_file = { enabled = true } },
+      })
+      vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+    end,
+  },
+})
+
---
+++ install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# THIS IS AN AUTOMATED SETUP SCRIPT FOR INSTALLING MY CODE EDITOR AND ITS PLUGINS.
+# THIS FILE IS MADE BY MICHAEL. NOT A 3RD PARTY THAT ISNT APART OF WT.
+# THIS FILE IS REPLACATABLE, ANY OTHER MEMBER OF THIS WT PROGRAMME CAN EXECUTE THIS TOO.
+# Installs the Brew Package Manager.
+# Installs Text Editor (Neovim), Terminal Editor (kitty), and git for installing the dotfiles repo
+# Clones my Git repo with all the config and UserData Files. 
+# There is no Student Info on this repo. only commands, themes, automations, etc.
+# Moves all of it's contents to my local profile
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install neovim git zsh
+brew install --cask kitty
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+chsh -s /bin/zsh
+
+cd ~/
+git clone https://github.com/kiestdev/kijest-dots.git
+cd kijest-dots
+
+mv -n config/ ~/.config/
+mv zshrc ~/.zshrc
+
+export EDITOR="/opt/homebrew/bin/kitty +kitten edit"
+export VISUAL="$EDITOR"
+
+cd ~/
+
+echo "INSTALLATION COMPLETE, NO ERROR ON FILE"
